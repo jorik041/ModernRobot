e0e0d43e30964be78ee4f31339fcebccd021bc28
Anton <anton@localhost>
2016-05-21 00:06:53 +0300

    Server logger.

---
+++ ModernServer/Helpers/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ModernServer.Helpers
+{
+    public class Logger
+    {
+        private string _logPath;
+
+        private void CheckLogFile()
+        {
+            var fInfo = new FileInfo(_logPath);
+            if (fInfo.Exists)
+            {
+                if (fInfo.Length / 1024 / 1024  >= 1)
+                {
+                    File.Move(_logPath, string.Format("{0}_{1}.backup.log", _logPath.Split('.').First(), DateTime.Now.Ticks));
+                    File.WriteAllLines(_logPath, new List<string>() { string.Format("Log continued at {0}", DateTime.Now) });
+                }
+            }
+        }
+
+        public Logger(string logPath)
+        {
+            _logPath = logPath;
+            Log("Started.");
+        }
+
+        public void Log(string info)
+        {
+            lock (this)
+            {
+                CheckLogFile();
+                using (var stream = new StreamWriter(_logPath, true))
+                {
+                    stream.WriteLine(string.Format("{0} {1}", DateTime.Now, info));
+                    stream.Close();
+                }
+            }
+        }
+    }
+}
---
+++ ModernServer/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernServer.Helpers;
+using System.IO;
+using System.Reflection;
+
+namespace ModernServer
+{
+    class Server
+    {
+        private static Logger logger;
+        private const string LOGSFOLDER = "Logs";
+        private const string LOGFILENAME = "modernServer.log";
+
+        private static void InitializeLogger()
+        {
+            var logFolder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), LOGSFOLDER);
+            if (!Directory.Exists(logFolder))
+                Directory.CreateDirectory(logFolder);
+            logger = new Logger(Path.Combine(logFolder, LOGFILENAME));
+        }
+
+        private static void Log(string contents)
+        {
+            Console.WriteLine(contents);
+            if (logger!=null)
+                logger.Log(contents);
+        }
+
+        static void Main(string[] args)
+        {
+            InitializeLogger();
+            
+        }
+    }
+}
